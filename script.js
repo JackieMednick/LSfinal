c70c54e9264285840cab10c2f750a406a032f637
JackieMednick <mednj302@newschool.edu>
2025-12-11 04:28:01 -0500

    images and others

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const buttons = document.querySelectorAll(".nav-button");
 
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
-        // Start fade-out
+
         document.body.style.opacity = "0";
 
-        // Wait for fade-out to finish, then navigate
         setTimeout(() => {
             window.location.href = btn.getAttribute("data-link");
-        }, 600); // matches the CSS transition
+        }, 600);
     });
+});
+
+document.querySelectorAll(".nav-button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        window.location.href = btn.getAttribute("data-link");
+    });
+});
+
+
+const smallImg = document.querySelector(".top-right-img");
+const overlay = document.getElementById("img-overlay");
+
+smallImg.addEventListener("click", () => {
+    overlay.style.display = "flex";
+});
+
+overlay.addEventListener("click", () => {
+    overlay.style.display = "none";
 });
